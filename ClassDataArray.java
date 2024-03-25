0a5e8e5c1c90e80a0acfe63a5c0fe73c39b016c5
jeikhan <evgenii.kapaev@yandex.ru>
2024-03-25 12:25:25 +0300

    ClassDataArray
    - Добавлен метод поиска пользователя по фамилии

---
+++ ClassDataArray.java
@@ -0,0 +1,21 @@
+public class ClassDataArray {
+    private Person[] a;
+    private int elements;
+
+    public ClassDataArray(int max) {
+        a = new Person[max];
+        elements = 0;
+    }
+    
+    public Person find(String searchLast) {
+        Person result = null;
+        int i;
+        for (i = 0; i < elements; i++) {
+            if (a[i].getLast().equals(searchLast)) {
+                result = a[i];
+                break;
+            }
+        }
+        return result;
+    }
+}
